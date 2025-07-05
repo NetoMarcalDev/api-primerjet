f6f46d411db7d6e490d5ad43cd90c515d113fc20
Cisco <neto.marcal.dev@gmail.com>
2025-07-05 16:43:51 -0300

    Adicionando o JwtStrategy e  PassportModule no providers e exports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersRepository } from './repositories/user.repository';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UsersRepository])],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController]
 })
 export class AuthModule {}
