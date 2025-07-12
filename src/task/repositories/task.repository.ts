b8835e627e065b9c61907cc9b1c6bfced37fda24
Cisco <neto.marcal.dev@gmail.com>
2025-07-12 13:41:39 -0300

    ImplementaçÕ D a feature createTask

--- src/task/repositories/task.repository.ts
+++ src/task/repositories/task.repository.ts
@@ -1,7 +1,20 @@
 import { Repository, EntityRepository } from 'typeorm';
 import { Task } from '../entities/task.entity';
+import { CreateTaskDto } from '../dto/create-task.dto';
+import { User } from 'src/auth/entities/user.entity';
+import { TaskStatus } from '../enums/task-status.enum';
 
 @EntityRepository(Task)
 export class TasksRepository extends Repository<Task> {
-    
+    async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
+    const { title, description } = createTaskDto;
+    const task = await this.create({
+      title,
+      description,
+      status: TaskStatus.ABERTO,
+    });
+
+    await this.save(task);
+    return task;
+  }
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TasksRepository } from './repositories/task.repository';
+import { Task } from './entities/task.entity';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { User } from 'src/auth/entities/user.entity';
 
 @Injectable()
 export class TaskService {
     constructor(
         @InjectRepository(TasksRepository)
         private tasksRepository: TasksRepository,
-    ) {}
+  ) {}
+
+    createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
+        return this.tasksRepository.createTask(createTaskDto, user);
+    }
 }
