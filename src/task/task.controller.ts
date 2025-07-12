ef33aac84be92930477926b4836b56047e2c27c3
Cisco <neto.marcal.dev@gmail.com>
2025-07-12 14:12:48 -0300

    Implementaçõa d a feature createTask.

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { TaskService } from './task.service';
+import { Task } from './entities/task.entity';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { GetUser } from 'src/auth/get-user.decorator';
+import { User } from 'src/auth/entities/user.entity';
 
 @Controller('task')
 export class TaskController {
     constructor( private taskService: TaskService ) {}
+
+    @Post()
+    createTask(
+        @Body() createTaskDto: CreateTaskDto,
+        @GetUser() user: User,
+    ) : Promise<Task> {
+        return this.taskService.createTask(createTaskDto, user);
+    }
+
 }
