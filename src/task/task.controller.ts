4042b33339a42786ea88ea7abbaa6395f5d75e1b
Cisco <neto.marcal.dev@gmail.com>
2025-07-13 09:06:13 -0300

    Implementação do AuthModule.

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,10 +1,12 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { Task } from './entities/task.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetUser } from 'src/auth/get-user.decorator';
 import { User } from 'src/auth/entities/user.entity';
+import { AuthGuard } from '@nestjs/passport';
 
+@UseGuards(AuthGuard())
 @Controller('task')
 export class TaskController {
     constructor( private taskService: TaskService ) {}
--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -3,9 +3,10 @@ import { TaskController } from './task.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksRepository } from './repositories/task.repository';
 import { TaskService } from './task.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TasksRepository])],
+  imports: [TypeOrmModule.forFeature([TasksRepository]), AuthModule],
   controllers: [TaskController],
   providers: [TaskService]
 })
