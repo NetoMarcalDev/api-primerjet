68c8a665295ae1d23da261b016352753d5ff1561
Cisco <neto.marcal.dev@gmail.com>
2025-08-03 19:36:06 -0300

    Implementação do getTasks no controller.

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { Task } from './entities/task.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetUser } from 'src/auth/get-user.decorator';
 import { User } from 'src/auth/entities/user.entity';
 import { AuthGuard } from '@nestjs/passport';
+import { GetTaskFilterDto } from './dto/get-task-filter.dto';
 
 @UseGuards(AuthGuard())
 @Controller('task')
@@ -18,5 +19,14 @@ export class TaskController {
     ) : Promise<Task> {
         return this.taskService.createTask(createTaskDto, user);
     }
+
+    @Get()
+    getTask(
+        @Query() filterDto: GetTaskFilterDto,
+        @GetUser() user: User,
+
+    ): Promise<Task[]> {
+        return this.taskService.getTask(filterDto, user);
+    };
 
 }
