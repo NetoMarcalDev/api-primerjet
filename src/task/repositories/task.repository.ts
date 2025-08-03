b074f41cdeadaf23e5f2eeb3344e22e35b720979
Cisco <neto.marcal.dev@gmail.com>
2025-08-03 17:59:30 -0300

    Implementação do getTasks no repositório.

--- src/task/repositories/task.repository.ts
+++ src/task/repositories/task.repository.ts
@@ -4,6 +4,7 @@ import { CreateTaskDto } from '../dto/create-task.dto';
 import { User } from 'src/auth/entities/user.entity';
 import { TaskStatus } from '../enums/task-status.enum';
 import { ConflictException } from '@nestjs/common';
+import { GetTaskFilterDto } from '../dto/get-task-filter.dto';
 
 @EntityRepository(Task)
 export class TasksRepository extends Repository<Task> {
@@ -25,5 +26,25 @@ async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
        throw new ConflictException("Erro ao cadastrar tarefa", error);
     }
   }    
+  }
+
+  async getTasks(filterDto: GetTaskFilterDto, user: User): Promise<Task[]> {
+    const { status, search } = filterDto;
+    const query = this.createQueryBuilder('task');
+    query.where({ user });
+
+    if (status) {
+      query.andWhere('task.status = :status', { status });
+    }
+
+    if (search) {
+      query.andWhere(
+        '(LOWER(task.title) LIKE LOWER(:search) OR LOWER(task.description) LIKE LOWER(:search))',
+        { search: `%${search}%` },
+      );
+    }
+
+    const tasks = await query.getMany();
+    return tasks;
   }
 }
